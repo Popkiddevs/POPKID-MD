d731f557ce21cccd9aa098c47ab4dfce0682424f
Popkid <popkidtechs@gmail.com>
2024-10-06 02:07:14 +0300

    ⭕POP KID⭕

--- scs/bmworder/popkidcrick.js
+++ scs/bmworder/popkidcrick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *BMW MD*\n`;
+      formattedResult += `│⿻   *POPKID MD*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
