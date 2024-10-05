2f49dae34c14fda633977314249a6ad327d1a787
Popkid <popkidtechs@gmail.com>
2024-10-06 02:43:11 +0300

    ⭕POP KID⭕

--- scs/bmworder/tag.js
+++ scs/bmworder/tag.js
@@ -23,7 +23,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
     if (!botAdmin) return m.reply("*📛 BOT MUST BE AN ADMIN TO USE THIS COMMAND*");
     if (!senderAdmin) return m.reply("*📛 YOU MUST BE AN ADMIN TO USE THIS COMMAND*");
     // Extract the message to be sent
-    let message = `乂 *Attention Everyone* 乂\n\n*Message:* ${m.body.slice(prefix.length + cmd.length).trim() || 'no message'}\n\n`;
+    let message = `✇ 😇𝗣𝗢𝗣𝗞𝗜𝗗 𝗠𝗗 𝗧𝗔𝗚😇 ✇\n\n*Message:* ${m.body.slice(prefix.length + cmd.length).trim() || 'no message'}\n\n`;
         
 
 
