cc0074913002b21643a9a3461927c93aa1dd1d3e
Popkid <popkidtechs@gmail.com>
2024-10-08 12:22:01 +0300

    ⭕POP KID⭕

--- scs/bmworder/popkidtik.js
+++ scs/bmworder/popkidtik.js
@@ -76,10 +76,10 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
             },
             interactiveMessage: proto.Message.InteractiveMessage.create({
               body: proto.Message.InteractiveMessage.Body.create({
-                text: `Bmw TikTok Download\n\nTitle: ${currentResult.data.title}\nAuthor: ${currentResult.data.author.nickname}\nViews: ${currentResult.data.view}\nDuration: ${currentResult.data.duration}s\n`
+                text: `POPKID TikTok Download\n\nTitle: ${currentResult.data.title}\nAuthor: ${currentResult.data.author.nickname}\nViews: ${currentResult.data.view}\nDuration: ${currentResult.data.duration}s\n`
               }),
               footer: proto.Message.InteractiveMessage.Footer.create({
-                text: "© Ibrahim Adams"
+                text: "© Popkid"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
                  ...(await prepareWAMessageMedia({ image: { url: `` } }, { upload: Matrix.waUploadToServer })),
